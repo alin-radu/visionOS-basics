71367da48a31901957f39884ada10ade1b5eb258
Alin RADU <alin.radu.cristian@gmail.com>
2024-02-10 09:36:36 +0200

    Initial Commit

---
+++ VisionsOS-basics-visionos1/VisionsOS_basics_visionos1App.swift
@@ -0,0 +1,17 @@
+//
+//  VisionsOS_basics_visionos1App.swift
+//  VisionsOS-basics-visionos1
+//
+//  Created by Alin RADU on 10.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct VisionsOS_basics_visionos1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
